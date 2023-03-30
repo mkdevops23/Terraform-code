660956d7f561e483430822492d503defaec31987
mkdevops23 <129386453+mkdevops23@users.noreply.github.com>
2023-03-30 16:09:41 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,10 +9,4 @@ terraform {
       version = "3.6.0"
     }
   }
-backend "azurerm" {
-  resource_group_name   = "mk-terraform-infra"
-  storage_account_name  = "terraformstatemkls"
-  container_name        = "tstate"
-  key                   = "yPmyBmjRlJ9Fg9ryTN2jNR2J3Dnfgv6SCTktX9XaqkUxiqaV33rhsg515vloZq5/Ok+axjvwXvDj+ASt8LXbpw=="
-}  
 }
